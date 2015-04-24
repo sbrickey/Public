693bda303c67374ae17a891e2e1452a92523f0b9
SND\sbrickey_cp <SND\sbrickey_cp@e2d364d0-55ac-4a1a-ac35-e7051c898402>
2015-04-24 14:04:12 +0000

    Samples.SharePoint.BCS.DirectoryServices : initial check-in
    git-svn-id: https://sbrickey.svn.codeplex.com/svn@35299 e2d364d0-55ac-4a1a-ac35-e7051c898402

---
+++ Samples/SharePoint/Business Connectivity Services/DirectoryServices/DirectoryServices.Tests/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace DirectoryServices.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetAll_Check_Count()
+        {
+            // Arrange
+
+            // Act
+            var Results = SDS.SPConnectors.DirectoryServices.DomainControllers.GetAll();
+
+            // Assert
+            Results.Should().NotBeNull();
+            Results.Should().HaveCount(1);
+        }
+        [TestMethod]
+        public void GetAll_Check_FirstResult()
+        {
+            // Arrange
+
+            // Act
+            var Result = SDS.SPConnectors.DirectoryServices.DomainControllers.GetAll().FirstOrDefault();
+
+            // Assert
+            Result.Should().NotBeNull();
+            Result.HostName.ToLower().Should().StartWith("server");
+            Result.HostName.ToLower().Should().EndWith("domain.local");
+            Result.IPAddress.Should().NotBeEmpty();
+            Result.Site.Should().NotBeEmpty();
+        }
+    }
+}
---
+++ Samples/SharePoint/Business Connectivity Services/DirectoryServices/DirectoryServices/DomainController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+
+namespace SDS.SPConnectors.DirectoryServices
+{
+    public class DomainControllers
+    {
+        public struct DomainController
+        {
+            public string HostName;
+            public string IPAddress;
+            public string Site;
+        }
+
+        public static DomainController[] GetAll()
+        {
+            return Domain.GetComputerDomain()
+                         .DomainControllers.Cast<System.DirectoryServices.ActiveDirectory.DomainController>()
+                         .Select(dc => new DomainController() { 
+                             HostName = dc.Name,
+                             IPAddress = dc.IPAddress,
+                             Site = dc.SiteName
+                         })
+                         .ToArray();
+        } // IEnum<DomainController> GetAll()
+
+        public static DomainController GetByName(string HostName)
+        {
+            return Domain.GetComputerDomain()
+                         .DomainControllers.Cast<System.DirectoryServices.ActiveDirectory.DomainController>()
+                         .Select(dc => new DomainController()
+                         {
+                             HostName = dc.Name,
+                             IPAddress = dc.IPAddress,
+                             Site = dc.SiteName
+                         })
+                         .Single(dc => dc.HostName == HostName);
+        } // DomainController GetByName(...)
+
+    } // class
+} // namespace
