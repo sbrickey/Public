d6e5ba9e2a4aa533a9de022559f14c8934733eab
SND\sbrickey_cp <SND\sbrickey_cp@e2d364d0-55ac-4a1a-ac35-e7051c898402>
2015-11-23 14:48:56 +0000

    Libraries/Generics: - SynchronizationContext : add
    git-svn-id: https://sbrickey.svn.codeplex.com/svn@36746 e2d364d0-55ac-4a1a-ac35-e7051c898402

---
+++ Libraries/Generics/SynchronizationContextExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace SBrickey.Libraries.Generics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Provides typed Post/Send delegate execution on a specific synchronization context
+    /// </summary>
+    public static class SynchronizationContextExtensions
+    {
+
+        public delegate void SendOrPostCallback<T>(T inp);
+
+        /// <summary>
+        /// Asynchronous execution of a delegate, on the context's thread
+        /// </summary>
+        public static void Post<T>(this SynchronizationContext ctx, SendOrPostCallback<T> delegateCode, T typedObj)
+        {
+            ctx.Post(
+                d: (objParam) => { delegateCode((T)objParam); },
+                state: typedObj
+            );
+        }
+        /// <summary>
+        /// Synchronous execution of a delegate, on the context's thread
+        /// </summary>
+        public static void Send<T>(this SynchronizationContext ctx, SendOrPostCallback<T> delegateCode, T typedObj)
+        {
+            ctx.Send(
+                d: (objParam) => { delegateCode((T)objParam); },
+                state: typedObj
+            );
+        }
+
+    } // class
+} // namespace
