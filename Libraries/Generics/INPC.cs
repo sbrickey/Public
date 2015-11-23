c2a140e5b17b334ce7a717cf04aa22b73d20976d
SND\sbrickey_cp <SND\sbrickey_cp@e2d364d0-55ac-4a1a-ac35-e7051c898402>
2015-11-23 14:01:03 +0000

    Libraries / Generics: - INPC : add generic implementation template - RelayCommand : add
    git-svn-id: https://sbrickey.svn.codeplex.com/svn@36742 e2d364d0-55ac-4a1a-ac35-e7051c898402

---
+++ Libraries/Generics/INPC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace NYOCA.Client.Common.Generics
+{
+    abstract public class MyNPC : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(name));
+        }
+    }
+} // namespace
