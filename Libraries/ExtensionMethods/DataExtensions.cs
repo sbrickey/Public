44a23cbaf8aa648b1ce18ea7c0f95876550e6b15
SND\sbrickey_cp <SND\sbrickey_cp@e2d364d0-55ac-4a1a-ac35-e7051c898402>
2015-11-23 15:08:49 +0000

    Libraries/ExtensionMethods: - DataExtensions : new, for obtaining typed data with light rule validation (required, nullable, etc)
    git-svn-id: https://sbrickey.svn.codeplex.com/svn@36748 e2d364d0-55ac-4a1a-ac35-e7051c898402

---
+++ Libraries/ExtensionMethods/DataExtensions.cs
@@ -0,0 +1,119 @@
+﻿namespace SBrickey.Libraries.ExtensionMethods
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Text;
+
+    public static class DataExtensions
+    {
+        public static T GetValue<T>(this IDataReader dr, string name) where T : class
+        {
+            var i = GetOrdinal(dr, name);
+            if (!i.HasValue)
+                return null;
+
+            if (dr.IsDBNull(i.Value))
+                return null;
+
+            return dr[i.Value] as T;
+        }
+        public static T GetValue<T>(this DataRow dr, string name) where T : class
+        {
+            var i = dr.GetOrdinal(name);
+            if (!i.HasValue)
+                return null;
+
+            if (dr.IsNull(i.Value))
+                return null;
+
+            return dr[i.Value] as T;
+        }
+
+        public static T GetValueOrException<T>(this IDataReader dr, string name) where T : struct
+        {
+            var i = GetOrdinal(dr, name);
+
+            if (!i.HasValue)
+                throw new KeyNotFoundException("Column [" + name + "] not found");
+
+            if (dr.IsDBNull(i.Value))
+                throw new ArgumentNullException(name);
+
+            var val = dr[i.Value];
+            return (T)System.Convert.ChangeType(val, typeof(T));
+        }
+        public static T GetValueOrException<T>(this DataRow dr, string name) where T : struct
+        {
+            var i = dr.GetOrdinal(name);
+
+            if (!i.HasValue)
+                throw new KeyNotFoundException("Column [" + name + "] not found");
+
+            if (dr.IsNull(i.Value))
+                throw new ArgumentNullException(name);
+
+            var val = dr[i.Value];
+            return (T)System.Convert.ChangeType(val, typeof(T));
+        }
+
+        public static Nullable<T> GetValueOrNull<T>(this IDataReader dr, string name) where T : struct
+        {
+            var i = GetOrdinal(dr, name);
+            if (!i.HasValue)
+                return null;
+            if (dr.IsDBNull(i.Value))
+                return null;
+
+            var val = dr[i.Value];
+            return (T)System.Convert.ChangeType(val, typeof(T));
+        }
+        public static Nullable<T> GetValueOrNull<T>(this DataRow dr, string name) where T : struct
+        {
+            var i = dr.GetOrdinal(name);
+            if (!i.HasValue)
+                return null;
+            if (dr.IsNull(i.Value))
+                return null;
+
+            var val = dr[i.Value];
+            return (T)System.Convert.ChangeType(val, typeof(T));
+        }
+
+
+        public static int? GetOrdinal(this IDataReader dr, string name)
+        {
+            var col = dr.GetSchemaTable()
+                     .Columns.Cast<DataColumn>()
+                     .Where(c => c.ColumnName == name)
+                     .ToList();
+
+            if (!col.Any())
+                return null;
+            //throw new KeyNotFoundException("Column [" + name + "] not found");
+
+            if (col.Count > 1)
+                throw new DuplicateNameException("Duplicate columns with name [" + name + "] were found");
+
+            return col.Single().Ordinal;
+        }
+        public static int? GetOrdinal(this DataRow dr, string name)
+        {
+            var col = dr.Table
+                     .Columns.Cast<DataColumn>()
+                     .Where(c => c.ColumnName == name)
+                     .ToList();
+
+            if (!col.Any())
+                return null;
+                //throw new KeyNotFoundException("Column [" + name + "] not found");
+
+            if (col.Count > 1)
+                throw new DuplicateNameException("Duplicate columns with name [" + name + "] were found");
+
+            return col.Single().Ordinal;
+        }
+
+    } // class
+} // namespace
