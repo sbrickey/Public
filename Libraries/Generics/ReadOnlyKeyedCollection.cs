99b17efc8ea805b940d672ffb8fb5bf0c6fb1df0
SND\sbrickey_cp <SND\sbrickey_cp@e2d364d0-55ac-4a1a-ac35-e7051c898402>
2015-11-23 14:22:23 +0000

    Libraries / Generics: - ReadOnlyKeyedCollection : add
    git-svn-id: https://sbrickey.svn.codeplex.com/svn@36744 e2d364d0-55ac-4a1a-ac35-e7051c898402

---
+++ Libraries/Generics/ReadOnlyKeyedCollection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBrickey.Libraries.Generics
+{
+    /// <summary>
+    /// Provides a readonly dictionary with typed item selector
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    public class ReadOnlyKeyedCollection<TValue, TKey> : ReadOnlyCollection<TValue>
+    {
+        private readonly Dictionary<TKey, int> Index;
+        public ReadOnlyKeyedCollection(IList<TValue> items, Func<TValue, TKey> keySelector) : base(items)
+        {
+            Index = items.ToDictionary(
+                keySelector: i => keySelector(i),
+                elementSelector: i => this.Items.IndexOf(i)
+            );
+        }
+        
+        public TValue this[TKey key]
+        {
+            get
+            {
+                return this.Items[this.Index[key]];
+            }
+        }
+
+    } // class
+
+    /// <summary>
+    /// Provides a readonly dictionary with typed item selectors
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TKey1"></typeparam>
+    /// <typeparam name="TKey2"></typeparam>
+    public class ReadOnlyKeyedCollection<TValue, TKey1, TKey2> : ReadOnlyCollection<TValue>
+    {
+        private readonly Dictionary<TKey1, int> Key1Index;
+        private readonly Dictionary<TKey2, int> Key2Index;
+        public ReadOnlyKeyedCollection(IList<TValue> items, Func<TValue, TKey1> key1Selector, Func<TValue, TKey2> key2Selector)
+            : base(items)
+        {
+            Key1Index = items.ToDictionary(
+                keySelector: i => key1Selector(i),
+                elementSelector: i => this.Items.IndexOf(i)
+            );
+            Key2Index = items.ToDictionary(
+                keySelector: i => key2Selector(i),
+                elementSelector: i => this.Items.IndexOf(i)
+            );
+        }
+
+        public TValue this[TKey1 key]
+        {
+            get
+            {
+                return this.Items[this.Key1Index[key]];
+            }
+        }
+        public TValue this[TKey2 key]
+        {
+            get
+            {
+                return this.Items[this.Key2Index[key]];
+            }
+        }
+
+    } // class
+
+} // namespace
