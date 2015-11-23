af2c82615d41ff52356d18a5fbaa24da65fff24e
SND\sbrickey_cp <SND\sbrickey_cp@e2d364d0-55ac-4a1a-ac35-e7051c898402>
2015-11-23 15:14:22 +0000

    Libraries / Generics: - Cached<T> : add
    git-svn-id: https://sbrickey.svn.codeplex.com/svn@36750 e2d364d0-55ac-4a1a-ac35-e7051c898402

---
+++ Libraries/Generics/Cached.cs
@@ -0,0 +1,57 @@
+﻿namespace SBrickey.Libraries.Generics
+{
+    using System;
+
+    public class Cached<T> where T : class
+    {
+        private T cachedResponse = null;
+        private Func<T> factory;
+
+        // ctor
+        public Cached(Func<T> Factory)
+        {
+            if (Factory == null)
+                throw new ArgumentNullException("Factory");
+
+            this.factory = Factory;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (this.cachedResponse == null)
+                {
+                    cachedResponse = factory();
+                }
+                return cachedResponse;
+            }
+        }
+        
+        public void Invalidate() { this.cachedResponse = null; }
+        
+        public static explicit operator T(Cached<T> o) { return o.Value; }
+    } // class Cached
+
+    public class CachedAsync<T> where T : class
+    {
+        private T cachedResponse = null;
+        private readonly Action<T> AsyncDelegate = null;
+        private readonly Action<Action<T>> factory;
+
+        public CachedAsync(Action<Action<T>> FactoryInvokeAsync)
+        {
+            this.AsyncDelegate = new Action<T>((value) => { this.cachedResponse = value; });
+            this.factory = FactoryInvokeAsync;
+            this.factory(AsyncDelegate);
+        }
+
+
+        public T Value { get { return cachedResponse; } }
+
+        public void Invalidate() { this.cachedResponse = null; this.factory(this.AsyncDelegate); }
+
+        public static explicit operator T(CachedAsync<T> o) { return o.Value; }
+
+    } // class CachedAsync
+} // namespace
