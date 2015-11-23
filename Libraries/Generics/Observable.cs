8a31bc101db67239222f52b01c98c409f4498a74
SND\sbrickey_cp <SND\sbrickey_cp@e2d364d0-55ac-4a1a-ac35-e7051c898402>
2015-11-23 13:59:40 +0000

    Libraries / Generic: - update Observable to include NullableStruct
    git-svn-id: https://sbrickey.svn.codeplex.com/svn@36741 e2d364d0-55ac-4a1a-ac35-e7051c898402

--- Libraries/Generics/Observable.cs
+++ Libraries/Generics/Observable.cs
@@ -16,7 +16,7 @@ namespace SBrickey.Libraries.Generics
             public T NewValue { get; set; }
         }
 
-        public EventHandler<ChangedEventArgs> Changed;
+        public event EventHandler<ChangedEventArgs> Changed;
 
         public T Value
         {
@@ -24,12 +24,19 @@ namespace SBrickey.Libraries.Generics
 
             set
             {
-                if (!value.Equals(_value))
+                // if both null, do nothing
+                if (value == null && _value == null)
+                    return;
+
+                // if one is null and other is not, or values differ, set and fire
+                if ((value == null && _value != null) ||
+                    (value != null && _value == null) ||
+                    (!value.Equals(_value)))
                 {
                     T oldValue = _value;
                     _value = value;
 
-                    EventHandler<ChangedEventArgs> handler = Changed;
+                    var handler = Changed;
                     if (handler != null)
                         handler(this, new ChangedEventArgs
                         {
@@ -39,10 +46,17 @@ namespace SBrickey.Libraries.Generics
                 }
             }
         }
+
+        // ctors
+        public Observable() { }
+        public Observable(Action<string> inpcDelegate, string name)
+        { this.Changed += (s, e) => { inpcDelegate(name); }; }
+        public Observable(Action<string> inpcDelegate, System.Linq.Expressions.Expression<Func<object>> member)
+        { this.Changed += (s, e) => { inpcDelegate((member.Body as System.Linq.Expressions.MemberExpression).Member.Name); }; }
     } // class Observable
 
 
-    public class ObservableStruct<T> where T : struct
+    public class ObservableNullableStruct<T> where T : struct
     {
         private T? _value;
 
@@ -52,7 +66,7 @@ namespace SBrickey.Libraries.Generics
             public T? NewValue { get; set; }
         }
 
-        public EventHandler<ChangedEventArgs> Changed;
+        public event EventHandler<ChangedEventArgs> Changed;
 
         public T? Value
         {
@@ -74,6 +88,73 @@ namespace SBrickey.Libraries.Generics
                         });
                 }
             }
+        } // Value
+
+        // ctors
+        public ObservableNullableStruct() { }
+        public ObservableNullableStruct(Action<string> inpcDelegate, string name)
+        { this.Changed += (s, e) => { inpcDelegate(name); }; }
+        public ObservableNullableStruct(Action<string> inpcDelegate, System.Linq.Expressions.Expression<Func<object>> member)
+        { this.Changed += (s, e) => { inpcDelegate((member.Body as System.Linq.Expressions.MemberExpression).Member.Name); }; }
+        public ObservableNullableStruct(T DefaultValue, Action<string> inpcDelegate, System.Linq.Expressions.Expression<Func<object>> member)
+        {
+            this._value = DefaultValue;
+            this.Changed += (s, e) => { inpcDelegate((member.Body as System.Linq.Expressions.MemberExpression).Member.Name); };
+        }
+    } // class ObservableNullableStruct
+
+
+    public class ObservableStruct<T> where T : struct
+    {
+        private T _value;
+
+        public class ChangedEventArgs : EventArgs
+        {
+            public T OldValue { get; set; }
+            public T NewValue { get; set; }
+        }
+
+        public event EventHandler<ChangedEventArgs> Changed;
+
+        public T Value
+        {
+            get { return _value; }
+
+            set
+            {
+                if (!value.Equals(_value))
+                {
+                    T oldValue = _value;
+                    _value = value;
+
+                    var handler = Changed;
+                    if (handler != null)
+                        handler(this, new ChangedEventArgs
+                        {
+                            OldValue = oldValue,
+                            NewValue = _value
+                        });
+                }
+            }
+        } // Value
+
+        // ctors
+        public ObservableStruct() { }
+
+        public ObservableStruct(Action<string> inpcDelegate, string name)
+        { this.Changed += (s, e) => { inpcDelegate(name); }; }
+        public ObservableStruct(Action<string> inpcDelegate, string name, T DefaultValue = default(T))
+        {
+            this._value = DefaultValue;
+            this.Changed += (s, e) => { inpcDelegate(name); };
+        }
+
+        public ObservableStruct(Action<string> inpcDelegate, System.Linq.Expressions.Expression<Func<object>> member)
+        { this.Changed += (s, e) => { inpcDelegate((member.Body as System.Linq.Expressions.MemberExpression).Member.Name); }; }
+        public ObservableStruct(Action<string> inpcDelegate, System.Linq.Expressions.Expression<Func<object>> member, T DefaultValue = default(T))
+        {
+            this._value = DefaultValue;
+            this.Changed += (s, e) => { inpcDelegate((member.Body as System.Linq.Expressions.MemberExpression).Member.Name); };
         }
     } // class ObservableStruct
 } // namespace
